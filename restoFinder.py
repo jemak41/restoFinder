e4e92a28ee306e7eb4b5ddcf27a2571b5dd48b45
Jemak <jmacposadas@gmail.com>
2019-01-17 20:41:51 +0800

    Update restoFinder.py

--- restoFinder.py
+++ restoFinder.py
@@ -1,17 +1,48 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 import time
+import nexmo
+import restoConfig as rc
 
 url = 'https://eatigo.com'
-search_items = ['Korean','Indian']
+client = nexmo.Client(key=rc.key, secret=rc.secret)
+messages = []
+txtCnt = 0
+text = ''
 
 def main():
+    global search_items
+    #search_item = input('Enter keyword: ')
+    search_items = rc.search_items
+
     getSoup()
 
+    y = 0
+    charLim = 125
+    charLimit = 125
+    print(text)
+    print(len(text))
+    if len(text) > 0:
+        txtList = [line.split(',') for line in text]
+        txtCnt = int(len(text) / charLim) + 1
+        for x in range(txtCnt):
+            messages.append('')
+            while y < len(txtList):
+                messages[x] += txtList[y][0]
+                if y == charLimit:
+                    charLimit += charLim
+                    break
+                y += 1
+
+    for a in range(len(messages)):
+        #print(str(a + 1) + 'st message: \n' + messages[a] + ' ' + str(len(messages[a])) + 'chars')
+        texting(messages[a])
+
+
+
 def getSoup():
-    
+
     for i in range(len(search_items)):
         driver = webdriver.Chrome()
         driver.get(url)
@@ -39,20 +70,31 @@ def getSoup():
         driver.close()
 
 def getDetails(soup):
-    
+    global text
+
     for a in soup.find_all('a', class_='list-card__body'):
-        print(a.find('div', class_='list-card__title').text)
+        text += a.find('div', class_='list-card__title').text
         x = a.find('div', class_='list-card__text')
-        print(x.text)
-        print(x.find_next('div', class_='list-card__text').find_next('div', class_='list-card__text').text)
-        print(a.find('span', class_='list-card__rating-text').text + ' Stars')
+        text += ', ' + x.text
+        text += ', @' + x.find_next('div', class_='list-card__text').find_next('div', class_='list-card__text').text
+        text += ', ' + a.find('span', class_='list-card__rating-text').text + ' Stars'
+        text += '\n'
         for a in soup.find_all('div', class_='discount-item__second'):
             for b in a.find('div'):
                 if '5' in b.string:
                     c = b.find_parent('div').find_parent('div').find_previous_sibling('div',
                                                                                       class_='discount-item__first').text
-                    print(c)
-                    print(b.string)
+                    text += c + ' '
+                    text += b.string + ' '
+        text += '\n'
+        text += '\n'
+
+def texting(msg):
+    client.send_message({
+        'from': 'Nexmo',
+        'to': rc.number,
+        'text': msg,
+    })
 
 if __name__ == '__main__':
     main()
